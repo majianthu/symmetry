d0e7f8f1df6d0346d741dd428ad6c506c29df933
MA Jian <majian1979@qq.com>
2025-05-17 21:49:23 +0800

    Add files via upload

--- sym1.r
+++ sym1.r
@@ -6,13 +6,13 @@ library(FamilyRank)
 
 n = 300
 ce1 = 0
-b1 = bh2 = bhc1 = bhc2 = bhi = bhi = cm = fm = hm = k2u = k2 = ks = sgn  = 0 
+b1 = bh2 = bhc1 = bhc2 = bhi = bhk = cm = fm = hm = k2u = k2 = ks = sgn  = 0 
 wcx = mgg = mi = moi = mok = nac1 = nac2 = nai = nak = rw = 0
 x11(); par(mfrow=c(3,3))
 for(i in 1:9){
-  #x = rbeta(n,10-i,i) # simulation 1
-  #x = rALD(n, p=i/10) # simulation 2
-  x = rbinorm(n,0,5,1,1,i/10) # simulation 3
+  # x = rbeta(n,10-i,i) # simulation 1
+  x = rALD(n, p=i/10) # simulation 2
+  # x = rbinorm(n,0,5,1,1,i/10) # simulation 3
   hist(x,100)
   
   x1 = matrix(x-mean(x),n,1)
@@ -24,9 +24,10 @@ for(i in 1:9){
   bhc1[i] = BHC1(x,k)
   bhc2[i] = BHC2(x,k)
   bhi[i] = BHI(x)
+  bhk[i] = BHK(x)
   cm[i] = CM(x)
   fm[i] = FM(x)
-  hm[i] = HM(x,k)
+  #hm[i] = HM(x,k)
   k2u[i] = K2U(x)
   k2[i] = K2(x)
   ks[i] = KS(x)
@@ -44,18 +45,19 @@ for(i in 1:9){
 }
 x11(width = 8, height = 12)
 par(mfrow=c(6,4))
-#x1 = 1:9; xlab1 = "b" # simulation 1
-# x1 = seq(0.1,0.9,by=0.1); xlab1 = "k" # simulation 2
-x1 = seq(0.1,0.9,by=0.1); xlab1 = "p" # simulation 3
+# x1 = 1:9; xlab1 = "b" # simulation 1
+x1 = seq(0.1,0.9,by=0.1); xlab1 = "k" # simulation 2
+# x1 = seq(0.1,0.9,by=0.1); xlab1 = "p" # simulation 3
 plot(x1,ce1,main="CE",xlab = xlab1,ylab = "stats");lines(x1,ce1)
 plot(x1,b1,main="B1",xlab = xlab1,ylab = "stats");lines(x1,b1)
 plot(x1,bh2,main="BH2",xlab = xlab1,ylab = "stats");lines(x1,bh2)
 plot(x1,bhc1,main="BHC1",xlab = xlab1,ylab = "stats");lines(x1,bhc1)
 plot(x1,bhc2,main="BHC2",xlab = xlab1,ylab = "stats");lines(x1,bhc2)
 plot(x1,bhi,main="BHI",xlab = xlab1,ylab = "stats");lines(x1,bhi)
+plot(x1,bhk,main="BHK",xlab = xlab1,ylab = "stats");lines(x1,bhk)
 plot(x1,cm,main="CM",xlab = xlab1,ylab = "stats");lines(x1,cm)
 plot(x1,fm,main="FM",xlab = xlab1,ylab = "stats");lines(x1,fm)
-plot(x1,hm,main="HM",xlab = xlab1,ylab = "stats");lines(x1,hm)
+#plot(x1,hm,main="HM",xlab = xlab1,ylab = "stats");lines(x1,hm)
 plot(x1,k2u,main="K2U",xlab = xlab1,ylab = "stats");lines(x1,k2u)
 plot(x1,k2,main="K2",xlab = xlab1,ylab = "stats");lines(x1,k2)
 plot(x1,ks,main="KS",xlab = xlab1,ylab = "stats");lines(x1,ks)
